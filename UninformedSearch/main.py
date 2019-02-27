de45e7b1000333ed375c7c89c6190b10264d952b
John Cormican <jcormica@tcd.ie>
2019-02-27 00:11:57 +0000

    Added testing for dfs and bfs

--- UninformedSearch/main.py
+++ UninformedSearch/main.py
@@ -8,9 +8,16 @@ Created on Sun Feb 24 23:34:19 2019
 
 from Networks import Network
 
+RomaniaMap = Network(4)
 
-Romania = Network(4)
+RomaniaMap.Randomize()
 
-Romania.Randomize()
+print(RomaniaMap.con_matrix)
 
-print(Romania.con_matrix)
+#RomaniaMap.plot()
+
+pathd = RomaniaMap.DepthFirstSearch(0,2)
+pathb = RomaniaMap.BreadthFirstSearch(0,2)
+
+print("Using depth first, path is",pathd)
+print("Using breadth first, path is",pathb)
