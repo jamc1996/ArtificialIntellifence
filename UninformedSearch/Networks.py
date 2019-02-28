2daa99ced8db6ae783c07610028f907a9f66a125
John Cormican <jcormica@tcd.ie>
2019-02-28 00:26:54 +0000

    Fixed up Uniform Cost Search.

--- UninformedSearch/Networks.py
+++ UninformedSearch/Networks.py
@@ -14,16 +14,97 @@ import networkx as nx
 
 
 
-class Network:
-    """Represents any network of interconnected nodes.
-    attributes: num_nodes, A."""
-    
-
+class PricedNetwork():
     def __init__(self, num_nodes):
         self.num_nodes = num_nodes
         self.con_matrix = np.empty([num_nodes,num_nodes])
     
+    def Randomize(self, max_cost):
+        for i in range(self.num_nodes):
+            for j in range(self.num_nodes):
+                self.con_matrix[i][j] = random.randrange(0,max_cost+1)
+                
+    def Connect(self,i,j, cost = 1):
+        self.con_matrix[i][j] = cost
+        
+    def DisConnect(self,i,j):
+        self.con_matrix[i][j] = 0
+        
+    def DoubleConnect(self,i,j, cost_ij, cost_ji):
+        self.con_matrix[i][j] = cost_ij
+        self.con_matrix[j][i] = cost_ji  
     
+    def DoubleDisConnect(self,i,j):
+        self.con_matrix[i][j] = 0
+        self.con_matrix[j][i] = 0
+    
+    def _ExpandNode(self, place, null_elems, stack):
+        node = stack[place][1]
+        cost = stack[place][0]
+        for i in range(self.con_matrix.shape[0]):
+            if ((self.con_matrix[node][i] != 0) and (i not in null_elems)):
+                cost+=self.con_matrix[node][i]
+                new_pos = len(stack)
+                stack.append([cost, i])
+                for x in stack[place][1:]:
+                    stack[new_pos].append(x)
+                cost = stack[place][0]
+        del stack[place]
+        return stack
+    
+    def FindMinCost(self,stack):
+        place = 0
+        min_cost = stack[0][0]
+        for i in range(len(stack)):
+            if stack[i][0] < min_cost:
+                min_cost = stack[i][0]
+                place = i
+        return place
+            
+    def TreeSearch(self, start_node, goal_node, strategy,init_cost = 0):
+        stack = [[init_cost,start_node]]
+        null_elems = list()
+        while (stack):
+            place =  strategy(stack)
+            node = stack[place][1]
+            if node == goal_node:
+                stack[place].reverse()
+                print("Goal", stack[place][0], "found at cost", stack[place][len(stack[place])-1],"!")
+                return stack[place]
+            null_elems.append(node)
+            stack = self._ExpandNode(place,null_elems,stack)
+        print("Goal not found :(")
+        return -1
+
+    def UniformCostSearch(self, start_node,goal_node):
+        return self.TreeSearch(start_node, goal_node, self.FindMinCost)
+    
+        
+class BinaryNetwork(PricedNetwork):
+    """Represents any network of interconnected nodes.
+    attributes: num_nodes, A."""
+    
+    
+    def __init__(self, num_nodes):
+        super(BinaryNetwork,self).__init__(num_nodes)
+    
+    def Randomize(self):
+        super(BinaryNetwork,self).Randomize(1)
+        
+    def Connect(self,i,j):
+        super(BinaryNetwork,self).Connect(i,j)
+        self.con_matrix[i][j] = 1
+        
+    def DisConnect(self,i,j):
+        super(BinaryNetwork,self).DisConnect(i,j)
+        
+    def DoubleDisConnect(self,i,j):
+        super(BinaryNetwork,self).DoubleDisConnect(i,j)
+        
+    def DoubleConnect(self,i,j):
+        super(BinaryNetwork.self).DoubleConnect(i,j)
+       
+
     # I need to get back to making the plot nice.
 # =============================================================================
 #     def plot(self):
@@ -57,19 +138,6 @@ class Network:
                     stack[new_pos].append(x)
         del stack[place]
         return stack
-                
-    def Connect(self,i,j):
-        self.con_matrix[i][j] = 1
-        
-    def DoubleConnect(self,i,j):
-        self.con_matrix[i][j] = 1
-        self.con_matrix[j][i] = 1
-                
-    def Randomize(self):
-
-        for i in range(self.num_nodes):
-            for j in range(self.num_nodes):
-                self.con_matrix[i][j] = random.randrange(0,2)
         
     def TreeSearch(self, start_node, goal_node,  strategy):
         """Basic Tree Search allows for choice of depth first 
@@ -81,7 +149,7 @@ class Network:
             node = stack[place][0]
             if node == goal_node:
                 stack[place].reverse()
-                print("Goal", node, "found!")
+                print("Goal", node, "found !")
                 return stack[place]
             null_elems.append(node)
             stack = self._ExpandNode(place, null_elems, stack)
@@ -98,40 +166,5 @@ class Network:
         return self.TreeSearch(start_node, goal_node, self._BF)
     
     def DepthFirstSearch(self, start_node,goal_node):
-        return self.TreeSearch(start_node, goal_node, self._DF)
-
-
-
-class PricedNetwork(Network):
-    def Randomize(self, max_cost):
-        for i in range(self.num_nodes):
-            for j in range(self.num_nodes):
-                self.A[i][j] = random.randrange(0,max_cost+1)
-                
-    def FindMinCost(stack):
-        place = stack[0][1]
-        min_cost = stack[0][0]
-        for i in range(len(stack)):
-            if stack[i][0]<min_cost:
-                min_cost = stack[i][0]
-                place = i
-        return place
-            
-    def UniformCostSearch(self, start_node, goal_node, strategy):
-        stack = [[start_node]]
-        null_elems = list()
-        
-        while (stack):
-            place =  strategy(stack)
-            node = stack[place]
-            if node == goal_node:
-                stack[node].reverse()
-                print("Goal", stack[node][1], "found at cost", stack[node][0],"!")
-                return stack[node]
-            null_elems.append(node)
-            self._Network__ExpandNode(node,null_elems,stack,self.con_matrix)
-
-        
-        
-        
+        return self.TreeSearch(start_node, goal_node, self._DF)     
         
